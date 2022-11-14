dce2346e810817e7dd07bba6d86e194df5fbe714
Stellareign <tuguran@mail.ru>
2022-11-14 21:34:00 +0300

    СТРОКИ, часть 1

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,21 @@ public class Main {
 
         System.out.println("Hello world!");
         String phone = "+7 960 415-75-37";
-        phone = phone.replace("-", "");
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
+        phone = phone.replace("-", "");// убрали -
+        phone = phone.replace(" ", "");// убрали пробел
+        phone = phone.replace("+", "");// убрали +
         if (phone.length() == 10) {
-            phone = '7' + phone;
+            phone = '7' + phone; ]// добавили 7
         } else if (phone.length() > 11) {
             throw new RuntimeException("Телефон слишком длинный)");
         } else if (phone.length() < 10) {
             throw new RuntimeException("Телефон слишком короткий");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') { // если в номере вместо 7 другая цифра
             throw new RuntimeException("Среди нас посторонний");
 
         }
         System.out.println("phone = " + phone);
-        String expectedPhone = "79604157537";
+        String expectedPhone = "79604157537";// задали нужный формат  номера
         if (phone.equals(expectedPhone)) {
             System.out.println("Усрпех!");
         } else {
